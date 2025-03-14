cbf95fc6b279ba1b14cd865df1909d36acc161d3
Kyle Krull <kdkrull@gmail.com>
2025-03-14 17:56:59 -0500

    Improved version

--- src/zsh/sode/find-sources/find-sources.sh
+++ src/zsh/sode/find-sources/find-sources.sh
@@ -1,11 +1,57 @@
 #!/usr/bin/env zsh
 
-pattern='index[.][jt]s'
-directories=()
-directories+=(bitovi.github.com)
-directories+=(kkrull.github.com)
+# https://stackoverflow.com/a/56311706/112682
+emulate -LR zsh
+set -euo pipefail
 
-#find kkrull.github.com bitovi.github.com -type f -name 'index[.][jt]s' -or -name 'index[.][jt]sx' | grep -v '[.]git/' | grep -v 'node_modules'
-find "${directories[@]}" -type f -name "$pattern" \
-  | grep -v '[.]git/' \
-  | grep -v 'node_modules'
+## Shared environment
+
+# export _SODE_HOME="${0:A:h}"
+export _SODE_INVOCATION="${0:t}"
+# export _SODE_INVOCATION="${_SODE_INVOCATION} add"
+
+## Local environment
+
+## Command
+
+function main() {
+  zparseopts -D -E \
+    -help=help_option
+
+  if [[ -n "$help_option" ]]
+  then
+    print_usage
+    exit 0
+  elif [[ $# == 0 ]]
+  then
+    echo "$_SODE_INVOCATION: Missing pattern"
+    exit 1
+  elif [[ $# == 1 ]]
+  then
+    echo "$_SODE_INVOCATION: Missing path(s)"
+    exit 1
+  fi
+
+  local pattern="$1" ; shift 1
+
+  find "$@" -type f -name "$pattern" \
+    | grep -v '[.]git/' \
+    | grep -v 'node_modules'
+}
+
+function print_usage() {
+  cat >&2 <<-EOF
+USAGE
+$_SODE_INVOCATION --help
+$_SODE_INVOCATION <pattern> <path> [...]
+
+OPTIONS
+--help        Show help
+EOF
+}
+
+## Main
+
+# Make sure the script exits, even if main doesn't
+# https://unix.stackexchange.com/a/449508/37734
+main "$@"; exit
