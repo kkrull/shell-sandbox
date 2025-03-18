3c6783a253de5ef1f076ace81595ae09a5a95e6d
Kyle Krull <kdkrull@gmail.com>
2025-03-18 13:18:35 -0500

    Reduce duplication in Makefiles

---
+++ make.d/paths-system.mk
@@ -0,0 +1,34 @@
+
+prefix ?= /usr/local
+exec_prefix ?= $(prefix)
+
+# Executable programs that users can run (including symlinks)
+bindir := $(exec_prefix)/bin
+
+# Executable programs to be run by other programs, in a subdirectory thereof
+libexecdir := $(exec_prefix)/libexec
+libexecdirpkg := $(libexecdir)/sode
+
+# Read-only architecture-independent data files, in a subdirectory thereof
+datarootdir := $(prefix)/share
+datadir := $(datarootdir)
+
+datadirpkg := $(datadir)/sode
+$(datadirpkg):
+	mkdir -p $(datadirpkg)
+
+.PHONY: debug-system-paths
+debug-system-paths:
+	$(info Paths:)
+	$(info - prefix: $(prefix))
+	$(info - exec_prefix: $(exec_prefix))
+
+	$(info - bindir: $(bindir))
+
+	$(info - libexecdir: $(libexecdir))
+	$(info - libexecdirpkg: $(libexecdirpkg))
+
+	$(info - datarootdir: $(datarootdir))
+	$(info - datadir: $(datadir))
+	$(info - datadirpkg: $(datadirpkg))
+	@:
